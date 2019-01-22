89e29b2a9d37d1d3f2335852c44ffeedc7c4d870
Sven Oliver Moll <svolli@svolli.de>
2019-01-22 23:33:54 +0100

    split img_load code to image functions and buffer functions

---
+++ img_buffer.c
@@ -0,0 +1,53 @@
+/*****************************************************************************
+* | File        :   img_buffer.c
+* | Author      :   SvOlli
+* | Function    :   image loading and converting using ImageMagick
+*****************************************************************************/
+
+#include "img_load.h"
+
+#include <string.h>
+
+
+void buffer_clean( uint8_t *buffer, const epd_api_t *epd, uint8_t bitplane )
+{
+   memset( buffer + (epd->datasize * bitplane), 0xff, epd->datasize );
+}
+
+
+void buffer_img( uint8_t *buffer, const epd_api_t *epd, uint8_t bitplane,
+                 MagickWand *m_wand, PixelWand *p_wand )
+{
+   uint16x2_t imagesize, imagepos, offset, disppos;
+   uint8_t    *linestart;
+   MagickPixelPacket pix;
+
+   imagesize.x = MagickGetImageWidth( m_wand );
+   imagesize.y = MagickGetImageHeight( m_wand );
+
+   /* get top left corner for image to display */
+   offset.x = (epd->dispsize.x - imagesize.x) / 2;
+   offset.y = (epd->dispsize.y - imagesize.y) / 2;
+
+   for( imagepos.y = 0; imagepos.y < imagesize.y; ++imagepos.y )
+   {
+      disppos.y = offset.y + imagepos.y;
+      linestart = buffer + (epd->datasize * bitplane) + (epd->linestep * disppos.y);
+      for( imagepos.x = 0; imagepos.x < imagesize.x; ++imagepos.x )
+      {
+         disppos.x = offset.x + imagepos.x;
+         uint8_t bitmask = 1 << (~(disppos.x) & 0x7);
+         MagickGetImagePixelColor( m_wand, imagepos.x, imagepos.y, p_wand );
+         PixelGetMagickColor( p_wand, &pix );
+         if( pix.red > 0.1 )
+         {
+            *(linestart + (disppos.x / 8)) |= bitmask;
+         }
+         else
+         {
+            *(linestart + (disppos.x / 8)) &= ~bitmask;
+         }
+      } 
+   }
+}
+
--- img_load.c
+++ img_load.c
@@ -6,8 +6,6 @@
 
 #include "img_load.h"
 
-#include <string.h>
-
  
 uint16x2_t aspect( uint16x2_t display, uint16x2_t image )
 {
@@ -102,47 +100,3 @@ float magick_greyscale( MagickPixelPacket *pix )
 {
    return 0.299 * pix->red + 0.587 * pix->green + 0.114 * pix->blue;
 }
-
-
-void buffer_clean( uint8_t *buffer, const epd_api_t *epd, uint8_t bitplane )
-{
-   memset( buffer + (epd->datasize * bitplane), 0xff, epd->datasize );
-}
-
-
-void buffer_img( uint8_t *buffer, const epd_api_t *epd, uint8_t bitplane,
-                 MagickWand *m_wand, PixelWand *p_wand )
-{
-   uint16x2_t imagesize, imagepos, offset, disppos;
-   uint8_t    *linestart;
-   MagickPixelPacket pix;
-
-   imagesize.x = MagickGetImageWidth( m_wand );
-   imagesize.y = MagickGetImageHeight( m_wand );
-
-   /* get top left corner for image to display */
-   offset.x = (epd->dispsize.x - imagesize.x) / 2;
-   offset.y = (epd->dispsize.y - imagesize.y) / 2;
-
-   for( imagepos.y = 0; imagepos.y < imagesize.y; ++imagepos.y )
-   {
-      disppos.y = offset.y + imagepos.y;
-      linestart = buffer + (epd->datasize * bitplane) + (epd->linestep * disppos.y);
-      for( imagepos.x = 0; imagepos.x < imagesize.x; ++imagepos.x )
-      {
-         disppos.x = offset.x + imagepos.x;
-         uint8_t bitmask = 1 << (~(disppos.x) & 0x7);
-         MagickGetImagePixelColor( m_wand, imagepos.x, imagepos.y, p_wand );
-         PixelGetMagickColor( p_wand, &pix );
-         if( pix.red > 0.1 )
-         {
-            *(linestart + (disppos.x / 8)) |= bitmask;
-         }
-         else
-         {
-            *(linestart + (disppos.x / 8)) &= ~bitmask;
-         }
-      } 
-   }
-}
-
